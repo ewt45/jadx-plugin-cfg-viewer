c526a3ab87c387eabcfc05c004825b6cb4844778
ewt45 <79033456+ewt45@users.noreply.github.com>
2025-12-25 19:46:15 +0800

    Update CfgViewerPlugin.java

--- src/main/java/jadx/plugins/viewer/cfg/CfgViewerPlugin.java
+++ src/main/java/jadx/plugins/viewer/cfg/CfgViewerPlugin.java
@@ -9,21 +9,30 @@ import jadx.api.plugins.JadxPluginInfo;
 
 
 /*
-1. 根目录 settings.gradle.kts 和 jadx-cli 的build.gradle.kts 添加该模块
-2. META-INF 中确保指向该类
+META-INF 中 指向该类
 
 创建临时文件时应该在 CfgViewerPlugin#tmpDir 中。
 插件设置参考 JavaConvertOptions。
 多语言：目前 plugin info 和 首选项 不支持实时刷新。就用 Locale.getDefault() 吧。（ jadx 的 locale 获取：mainWindow.getSettings().getLangLocale() ）
-
-图形库依赖用的 compileOnly, 注意在没获取到 guiContext 之前别用那些图形类（成员属性，函数返回值也不行）
+注意在没获取到 guiContext 之前别用那些图形类（成员属性，函数返回值也不行）
 
 如何显示 dot？本地 dot 命令创建 png？还是第三方 java swing 库解析 dot 并显示，还是网页？
 图片显示用 piccolo2d https://piccolo2d.org/learn/out-of-box-java.html
 
+图形库依赖用的 compileOnly,
+
+构建 jar：
+	1. git 添加 tag
+	2. github action 里输入 tag 生成 artifact
+	3. 创建 release, 添加 jar 文件，命名格式 <仓库名>-<版本号>.jar
+	4. 确保可以 从 cli/gui 安装: jadx plugins --install github:ewt45:jadx-plugin-cfg-viewer
+
+
+调试：
+	1. 复制到 jadx 项目中
+	2. 根目录 settings.gradle.kts 和 jadx-cli 的build.gradle.kts 添加该模块依赖
  */
 
-// TODO 最后看一眼官方 wiki 和 example 还有啥需要注意的没.
 // TODO MyListeners.imageFitNow 和 缩ImageZoom 缩放范围根据图片尺寸而定
 // TODO 等待官方版本更新：1.5.4 后 CfgJNode 里重写 hasContent 方法。等待支持多语言。
 
